564c0784050beeb74931ba8e5ee7c197a9d3b22d
DyakonovArtur <101309876+DyakonovArtur@users.noreply.github.com>
2022-05-19 15:52:47 +0900

    w

---
+++ Weather/Weather/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by Гость on 19.05.2022.
+//
+
+import UIKit
+import CoreLocation
+
+class ViewController: UIViewController {
+
+    let locationManager = CLLocationManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        startLocationManager()
+    }
+
+
+    func startLocationManager () {
+        locationManager.requestWhenInUseAuthorization()
+        
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+            locationManager.pausesLocationUpdatesAutomatically = false
+            locationManager.startUpdatingLocation()
+        }
+    }
+    
+}
+
+extension ViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let lastLocation = locations.last {
+            print(lastLocation.coordinate.latitude, lastLocation.coordinate.longitude)
+        }
+    }
+}
